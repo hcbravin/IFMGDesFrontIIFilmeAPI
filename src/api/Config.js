7b8549ac37349e7f4c4de24134c2833c67affa3a
hcbravin <hcbravin@hotmail.com>
2026-03-29 16:48:42 -0300

    Arquivo de configuração

---
+++ src/api/Config.js
@@ -0,0 +1,4 @@
+export const apiKey = ""; // APIKEY
+export const searchMovieURL = "https://api.themoviedb.org/3/search/movie";
+export const movieDetailsURL = "https://api.themoviedb.org/3/movie";
+export const imageBaseURL = "https://image.tmdb.org/t/p/w500";
